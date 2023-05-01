1799882e49f84d40f77c8b066d26d100702e725b
Joy <mmehedihasanjoyv@gmail.com>
2023-05-02 01:30:19 +0600

    added the first nextjs project and to page

---
+++ pages/blog.js
@@ -0,0 +1,11 @@
+import styles from '@/styles/Home.module.css'
+
+const Blog = () => {
+    return (
+        <div  className={`${styles.main}`}>
+          <h1>This is blog page</h1>  
+        </div>
+    );
+};
+
+export default Blog;<h1>This is blog page</h1>
---
+++ pages/news.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Home.module.css'
+const News = () => {
+    return (
+        <div  className={`${styles.main}`}>
+            <h2>This is news page</h2>
+        </div>
+    );
+};
+
+export default News;
